06ba5e43fd0460fd8ce236d205f1e93f5a27d432
Oswald De Riemaecker <oswald@continuous.lu>
2018-10-16 11:00:45 +0200

    Moving redis install

--- prep.sh
+++ prep.sh
@@ -3,7 +3,6 @@
 git checkout step-1
 ansible-playbook --inventory environments/development/inventory webserver.yml
 ansible-playbook --inventory environments/development/inventory database.yml
-ansible-playbook --inventory environments/development/inventory redis.yml
 git checkout step-2
 ansible-galaxy -r requirements.yml install
 ansible-playbook --inventory environments/staging/inventory webserver.yml
@@ -14,4 +13,5 @@ ansible-playbook --inventory environments/production/inventory webserver.yml
 ansible-playbook --inventory environments/production/inventory database.yml
 git checkout step-4
 ansible-galaxy -r requirements.yml install
+ansible-playbook --inventory environments/development/inventory redis.yml
 git checkout step-1
